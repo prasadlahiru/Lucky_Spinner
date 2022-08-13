b7451181e311ca9a8270a064279eb87b97dd407b
Prasad Kodithuwakku <lahirumu98@gmail.com>
2022-08-13 23:02:55 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace LuckySpinner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum3 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAttempt = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblTitle.Location = new System.Drawing.Point(19, 48);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(223, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Guess Numbers";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(119, 232);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 26);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(198, 112);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(97, 20);
+            this.txtNum.TabIndex = 2;
+            this.txtNum.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtNum.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGuess_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter your guessed number";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(69, 179);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(27, 29);
+            this.lblNum1.TabIndex = 4;
+            this.lblNum1.Text = "#";
+            // 
+            // lblNum3
+            // 
+            this.lblNum3.AutoSize = true;
+            this.lblNum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum3.Location = new System.Drawing.Point(203, 179);
+            this.lblNum3.Name = "lblNum3";
+            this.lblNum3.Size = new System.Drawing.Size(27, 29);
+            this.lblNum3.TabIndex = 5;
+            this.lblNum3.Text = "#";
+            this.lblNum3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(137, 179);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(27, 29);
+            this.lblNum2.TabIndex = 6;
+            this.lblNum2.Text = "#";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "No of attempts:";
+            // 
+            // lblAttempt
+            // 
+            this.lblAttempt.AutoSize = true;
+            this.lblAttempt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAttempt.ForeColor = System.Drawing.Color.Red;
+            this.lblAttempt.Location = new System.Drawing.Point(195, 281);
+            this.lblAttempt.Name = "lblAttempt";
+            this.lblAttempt.Size = new System.Drawing.Size(15, 16);
+            this.lblAttempt.TabIndex = 8;
+            this.lblAttempt.Text = "3";
+            this.lblAttempt.Click += new System.EventHandler(this.lblAttempt_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(340, 306);
+            this.Controls.Add(this.lblAttempt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum3);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Lucky Spinner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum3;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAttempt;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuckySpinner
+{
+    public partial class Form1 : Form
+    {
+        public int num1, num2, num3;
+        public int count = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            num1 = GetRandNo(0, 5);
+            num2 = GetRandNo(6, 10);
+            num3 = GetRandNo(11, 20);
+
+            int[] myarr = new int[3] { num1, num2, num3 };
+
+
+        }
+        private int GetRandNo(int min, int max)
+        {
+            Random rand = new Random();
+            int random = rand.Next(min, max);
+            return random;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewGame();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+          
+            if (txtNum.Text != "" && Convert.ToInt32(txtNum.Text)>50)
+            {
+                MessageBox.Show("Invalid Input!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNum.Clear();
+            }
+            else if (e.KeyCode == Keys.Enter && txtNum.Text != "")
+            {
+                GuessingProcess(Convert.ToInt32(txtNum.Text));
+                txtNum.Clear();
+
+            }
+            else if(e.KeyCode==Keys.Enter && txtNum.Text == "")
+            {
+                MessageBox.Show("Invalid Input!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void txtGuess_KeyPress(object sender, KeyPressEventArgs e)
+        {if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+               
+            }
+        
+        }
+        private void NewGame()
+        {
+            txtNum.Clear();
+            txtNum.Enabled = true;
+            count = 3;
+            lblAttempt.Text = Convert.ToString(count);
+            num1 = GetRandNo(0, 5);
+            num2 = GetRandNo(6, 10);
+            num3 = GetRandNo(11, 20);
+            lblNum1.Text = "#";
+            lblNum2.Text = "#";
+            lblNum3.Text = "#";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAttempt_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void GuessingProcess(int no)
+        {
+            string Message = "Sorry Game is over!";
+            string title = "Bad-Luck";
+          
+
+            if (count == 0)
+            {
+                txtNum.Enabled = false;
+             DialogResult result=   MessageBox.Show(Message, title, MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                if (result == DialogResult.Retry)
+                {
+                    // NewGame method should be called here;
+                    NewGame();
+                }
+            }
+            else if (count >=1)
+            {
+                if (no == num1)
+                {
+                    lblNum1.Text = Convert.ToString(no);
+                }
+                else if (no == num2)
+                {
+                    lblNum2.Text = Convert.ToString(no);
+                }
+                else if (no == num3)
+                {
+                    lblNum3.Text = Convert.ToString(no);
+
+                }
+                else if (no != num1 || no != num2 || no != num3)
+                {
+                    count--;
+                    lblAttempt.Text = Convert.ToString(count);
+                }
+                else if(lblNum1.Text==Convert.ToString(num1)&& lblNum2.Text==Convert.ToString(num2) && lblNum3.Text==Convert.ToString(num3))
+                {
+                 DialogResult result=MessageBox.Show("You Won the Game", "Great!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                    if (result == DialogResult.Retry)
+                    {
+                        //newGame method should execute here,.
+                        NewGame();
+                    }
+                
+                
+                }
+            }
+        }
+    }
+}
